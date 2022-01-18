b71e6dc2bcbaef97d698ded48d35bb2a7294aa18
Mark Zaianchkovksyi <marko4148@gmail.com>
2022-01-19 00:56:02 +0100

    Added type hints to models_manager

--- hello_db/models_manager.py
+++ hello_db/models_manager.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import select
@@ -7,22 +7,19 @@ from hello_db.models import User, HiBase
 from app import db_engine
 
 
-print(type(db_engine))
-
-
 class UserManager:
     def __init__(self, user_model=User, engine=db_engine, base=HiBase):
         self._user_model = user_model
         self._engine = engine
         self._base = base
 
-    def init_table(self):
+    def init_table(self) -> None:
         self._user_model.__table__.create(self._engine)
 
-    def drop_table(self):
+    def drop_table(self) -> None:
         self._user_model.__table__.drop(self._engine)
 
-    def select_all_users(self):
+    def get_all_users(self) -> List[User]:
         with Session(self._engine) as session:
             users = session.execute(select(self._user_model)).all()
 
