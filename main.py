c14f2e9485d6c0ff33c953597ec52d9320b3c87f
Fan4ik <marko4148@gmail.com>
2022-01-20 23:22:06 +0000

    R10 heroku problem fixed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+
 from app import app
 import views
 import management.commands
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
